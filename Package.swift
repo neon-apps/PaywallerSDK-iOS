5bed95dc8e67226ca2134961c02883a0775e39ae
Tunahan Öztürk <business@tunaozturk.com>
2024-03-10 16:15:52 +0300

    NeonSDK updated

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         
         
-        .package(name: "NeonSDK", url: "git@github.com/neon-apps/neon-sdk.git", branch: "main"),
+        .package(name: "NeonSDK", url: "https://github.com/neon-apps/neon-sdk.git", branch: "main"),
       
        
         // Dependencies declare other packages that this package depends on.
