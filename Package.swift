aa004cad79134562d26388310cde8e52a544e49a
Tunahan Öztürk <business@tunaozturk.com>
2024-03-11 12:16:32 +0300

    Version Updated

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Paywaller",   
     
     platforms: [
-    .iOS(.v12)
+    .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
